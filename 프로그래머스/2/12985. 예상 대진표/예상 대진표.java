bea76907a4d4f6e0f015c100d5446a2f0cc766cc
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-11-23 05:15:59 +0900

    [level 2] Title: 예상 대진표, Time: 0.22 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,17 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1; // 첫 라운드는 1
+        int answer = 0; // 라운드 수
 
-        // 로그를 이용하여 총 대전 가능 횟수만큼 반복
-        for(int i = 0; i < (int)(Math.log(n) / Math.log(2)) + 1; i++) { 
-            if(Math.max(a, b) - Math.min(a, b) == 1     // a와 b의 순번이 연속되면서
-                         && Math.min(a, b) % 2 == 1) {  // a와 b 중 작은 순번이 홀수일 때
-                break;
-            }
-            
-            a = (int)Math.ceil((double)a/2); // a의 새로운 순번
-            b = (int)Math.ceil((double)b/2); // b의 새로운 순번
+        /*
+            a의 대진과 b의 대진의 번호가 같을 때 서로 붙게 된다. 
+            따라서 다를 경우 반복하며 라운드 수를 증가시켜주면 된다.
+        */
+        while(a != b) { 
+            a = (int)Math.ceil((double)a/2); // a의 대진
+            b = (int)Math.ceil((double)b/2); // b의 대진
             answer++; // 라운드 증가
         }
 
