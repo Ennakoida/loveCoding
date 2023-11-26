6f9a577cb39cfa6ab4e4419508eda4b344a54f91
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-11-26 23:34:35 +0900

    Revert "[level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub"
    This reverts commit 595bcdcee72f5c9a90bea62865e03a1fddcec6a2.

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,6 +2,18 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        return Integer.toBinaryString((a-1)^(b-1)).length();
+        int answer = 0; // 라운드 수
+
+        /*
+            a의 대진과 b의 대진의 번호가 같을 때 서로 붙게 된다. 
+            따라서 다를 경우 반복하며 라운드 수를 증가시켜주면 된다.
+        */
+        while(a != b) { 
+            a = (int)Math.ceil((double)a/2); // a의 대진
+            b = (int)Math.ceil((double)b/2); // b의 대진
+            answer++; // 라운드 증가
+        }
+
+        return answer;
     }
 }
