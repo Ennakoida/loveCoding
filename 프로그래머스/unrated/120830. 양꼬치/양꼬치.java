726c3d4f6116006adb96fa50325d1a1eced586c6
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-01 22:55:38 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,5 @@
 class Solution {
-    public int solution(int n, int k) {
-        int answer = (12000 * n) + (2000 * k);
-        
-        if(n >= 10) {
-            answer -= 2000 * (n / 10);
-        }
-        
-        return answer;
+    public int solution(int n, int k) {        
+        return (12000 * n) + (2000 * k) - (2000 * (n / 10));
     }
 }
