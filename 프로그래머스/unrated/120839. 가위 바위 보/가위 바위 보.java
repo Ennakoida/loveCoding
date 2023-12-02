e8b248f5987b192cf1188dd96ce7336ba013f3f4
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-03 04:58:55 +0900

    [level unrated] Title: 가위 바위 보, Time: 2.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.java
@@ -3,25 +3,17 @@ class Solution {
         String answer = "";
         
         for(int i = 0; i < rsp.length(); i++) {
-            answer += result(rsp.charAt(i));
-        }
-        
-        return answer;
-    }
-    
-    public String result(char rsp) {
-        String answer = "";
-        
-        switch(rsp) {
-            case '2' :
-                answer = "0";
-                break;
-            case '0' :
-                answer = "5";
-                break;
-            case '5' :
-                answer = "2";
-                break;
+            switch(rsp.charAt(i)) {
+                case '2' :
+                    answer += "0";
+                    break;
+                case '0' :
+                    answer += "5";
+                    break;
+                case '5' :
+                    answer += "2";
+                    break;
+            }
         }
         
         return answer;
