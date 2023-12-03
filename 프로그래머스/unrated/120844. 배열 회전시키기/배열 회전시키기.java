75c2f7e3d45e98955e98b24764e8d56ba55f8114
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-04 02:39:31 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            if(direction.equals("left")) {
+                if(i + 1 > numbers.length - 1) {
+                    answer[i] = numbers[0];                    
+                } else {
+                    answer[i] = numbers[i + 1];    
+                }
+            } else {
+                if(i == 0) {
+                    answer[i] = numbers[numbers.length - 1];
+                } else {
+                    answer[i] = numbers[i - 1];    
+                }
+            }
+        }
+        return answer;
+    }
+}
