42bdafa4e339abf99250b9b23b38e7c55fe35e44
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-07 23:34:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.13 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
+        // ******* 1차 풀이 (효율성 시간 초과) *******
 //         for(int i = 0; i < completion.length; i++) {
 //             for(int j = 0; j < participant.length; j++) {
 //                 if(completion[i].equals(participant[j])) {
@@ -17,20 +19,32 @@ class Solution {
 //             if(participant[i] != "") answer = participant[i];
 //         }
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        // ******* 2차 풀이 (통과 but Hash 이용X) *******
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
         
-        for(int i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                answer = participant[i];
-                break;
-            } 
+//         for(int i = 0; i < completion.length; i++) {
+//             if(!participant[i].equals(completion[i])) {
+//                 answer = participant[i];
+//                 break;
+//             } 
+//         }
+        
+//         if(answer == "") {
+//             answer = participant[participant.length - 1];
+//         }
+        
+        // ******* 3차 풀이 (Hash 이용) *******
+        HashMap<String, Integer> hash = new HashMap<>();
+        
+        for(String key : participant) hash.put(key, hash.getOrDefault(key, 0) + 1);
+        for(String key : completion) hash.put(key, hash.get(key) - 1);
+        
+        for(String key : hash.keySet()) {
+            if(hash.get(key) != 0) answer = key;
         }
         
-        if(answer == "") {
-            answer = participant[participant.length - 1];
-        }
-        
+            
         return answer;
     }
 }
