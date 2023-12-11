1fd7fce211426e096558b04a0004718acb4b5f2f
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-12 06:21:50 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.38 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        
+        char[] str = my_string.toCharArray();
+        Arrays.sort(str);
+        
+        return new String(str);
+    }
+}
