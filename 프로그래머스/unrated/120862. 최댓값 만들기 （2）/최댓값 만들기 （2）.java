58971c35b2319add29f39b8df01cca899e1a6a20
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-14 17:27:02 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.38 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,12 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int multiple = 0;
         Arrays.sort(numbers);
         
-        multiple = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-        if(numbers[0] * numbers[1] > multiple) multiple = numbers[0] * numbers[1];
-        
-        return multiple;
+        return Math.max(numbers[numbers.length - 1] * numbers[numbers.length - 2], numbers[0] * numbers[1]);
     }
 }
