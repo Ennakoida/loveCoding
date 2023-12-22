91746813b2d835e0692f388b6a39d47f58707a4a
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-12-22 23:09:09 +0900

    [level 2] Title: H-Index, Time: 0.33 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -8,18 +8,11 @@ class Solution {
         
         for(int i = 0; i < citations.length; i++) {
            if(citations[i] >= citations.length - i) {
-               answer = citations.length - i ;   // citations[i];
+               answer = citations.length - i;
                break;
            }
-//             if(citations.length == 1) {
-// //                System.out.println("1");
-//                 return citations[i];
-//             } else if(citations[i] >= (int)((citations.length+1)/2.0)) 
-//             {
-// //                System.out.println(i + "," + citations[i] + "," + (int)((citations.length+1)/2.0));
-//                 return citations[i];
-//             }
         }
+        
         return answer;
     }
 }
