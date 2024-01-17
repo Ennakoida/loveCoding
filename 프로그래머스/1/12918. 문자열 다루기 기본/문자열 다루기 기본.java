2f24ab1301bb15dcac74c3c330f9b9881a1668d2
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-17 23:00:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
-        if ((s.length() == 4 || s.length() == 6) && s.matches("^[0-9]*$")) answer = true;
-        return answer;
+        if ((s.length() == 4 || s.length() == 6) && s.matches("^[0-9]*$")) return true;
+        return false;
     }
 }
