fb2f15efd98372b2e6ccb306952253e050d99875
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-24 18:02:24 +0900

    [level 1] Title: 삼총사, Time: 0.38 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -10,7 +10,7 @@ class Solution {
         for(int i = 0; i < number.length - 2; i++) {
             for(int j = i + 1; j < number.length - 1; j++) {
                 for(int k = j + 1; k < number.length; k++) {
-                   if(number[i] + number[j] == number[k] * (-1)) answer++;
+                   if(number[i] + number[j] + number[k] == 0) answer++;
                 }
             }
         }
