24dfe065d45f6ebb7bb067040301b0beba9238f3
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-24 18:55:39 +0900

    [level 1] Title: 최소직사각형, Time: 2.15 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,16 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int[] wSizes = new int[sizes.length];
-        int[] hSizes = new int[sizes.length];
+        int maxW = 0, maxH = 0;
         
         for(int i = 0; i < sizes.length; i++) {
-            wSizes[i] = Math.max(sizes[i][0], sizes[i][1]);
-            hSizes[i] = Math.min(sizes[i][0], sizes[i][1]);
+            maxW = Math.max(maxW, Math.max(sizes[i][0], sizes[i][1]));
+            maxH = Math.max(maxH, Math.min(sizes[i][0], sizes[i][1]));
         }
-        
-        int maxW = Arrays.stream(wSizes).max().getAsInt();
-        int maxH = Arrays.stream(hSizes).max().getAsInt();
         
         return maxW * maxH;
     }
