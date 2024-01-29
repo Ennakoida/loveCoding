90552194eb9e3c33cac9de2df86f069b2d9dfa92
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-29 22:13:00 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = n;
         
+        //Integer.bitCount(n) -> n의 자리에 정수 입력 시, n의 이진수에서 1의 갯수 반환
         while(true) {
             if(Integer.bitCount(n) == Integer.bitCount(++answer)) break;
         }
