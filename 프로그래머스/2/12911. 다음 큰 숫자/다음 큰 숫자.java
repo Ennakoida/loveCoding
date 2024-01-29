3d4846d48959e7f08f058094781238eb18862141
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-29 22:12:00 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n;
+        
+        while(true) {
+            if(Integer.bitCount(n) == Integer.bitCount(++answer)) break;
+        }
+        
+        return answer;
+    }
+}
