b3f6694e12044c4f0c163888c600b1fb5ca93a7a
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-01-30 21:24:56 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,14 +2,11 @@ class Solution {
     public String solution(int a, int b) {
         int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int date = 0;
         
         if(a > 1) {
             for(int i = 0; i < a - 1; i++) {
-                date += month[i];
+                b += month[i];
             }    
-            
-            b += date;
         } 
         
         return String.valueOf(day[(b - 1) % 7]);
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=java) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 0.04 ms
+메모리: 88.7 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 21:24:7
+2024년 1월 2일 21:24:54
 
 ### 문제 설명
 
