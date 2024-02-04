166c43677c0ede0e4143396b38b260a19399f61b
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2024-02-04 21:34:12 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0, sum = 0;
+        boolean check = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    check = true;
+
+                    for (int l = 2; l < sum; l++) {
+                        if (sum % l == 0) {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if (check) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
